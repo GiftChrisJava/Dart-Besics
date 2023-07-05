656b92bd7b4ab4b4828ac77c070bac4b434c196f
GiftChrisJava <chrisjava77@gmail.com>
2023-07-05 05:48:01 -0700

    added chapter 4

---
+++ chapter 4 - Strings/Strings.dart
@@ -0,0 +1,11 @@
+void main() {
+  // interpolation
+  const name = "Ray";
+  const introduction = "Hello my name is $name";
+  print(introduction);
+
+  // controlling the number of decimal numbers
+  const oneThird = 1 / 3;
+  final sentance = 'one third is ${oneThird.toStringAsFixed(3)}';
+  print(sentance);
+}
---
+++ chapter 4 - Strings/exercise1.dart
@@ -0,0 +1,9 @@
+void main() {
+  const firstName = "Gift Chris";
+  const lastName = "Zulu";
+  const fullName = firstName + " " + lastName;
+  print(fullName.toUpperCase());
+
+  const myDetails = "Hello my name is $fullName";
+  print(myDetails);
+}
