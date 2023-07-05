367bef3ca33adce704f0a2d5df276a63266454f6
GiftChrisJava <chrisjava77@gmail.com>
2023-07-05 09:24:09 -0700

    done with strings

--- chapter 4 - Strings/multipleStrings.dart
+++ chapter 4 - Strings/multipleStrings.dart
@@ -1,1 +1,18 @@
+void main() {
+  // multi-line string
+  const bigString = '''
+  you have a string 🎯
+  that constains multiple ❤
+  lines 
+  by
+  doing this. ''';
 
+  print(bigString);
+
+  //ignoring any spacial charecters
+  const rawString = r"My name is \n Jone";
+  print(rawString);
+
+  print('I \u2764 Dart\u0021');
+  print('I love \u{1F3AF}');
+}
